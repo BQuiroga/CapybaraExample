79b058a152cb560a86c5c07252e52b2cd6ba3f04
sergioKmoralesV <s.mora.94@gmail.com>
2017-06-07 05:51:04 -0400

    Add preferences steps

---
+++ features/step_definitions/3PreferencesSteps.rb
@@ -0,0 +1,40 @@
+
+When(/^Press the button "([^"]*)" in homepage$/) do |buttonName|
+    find('a',:text => buttonName).click
+end
+
+Then(/^I should see the page for logging in into Trivago$/) do
+    find(:xpath,'//*[@id="authentication-login"]/div/section/div[1]/div/h2', :text => 'Log in with a social network')
+    find(:xpath,'//*[@id="authentication-login"]/div/section/div[3]/div/h2', :text => 'Log in with your email')
+    find('input', :id => 'emailLogin')
+    find('input', :id => 'login-pass')
+end
+
+Given(/^I am on trivago login page$/) do
+    visit 'https://accounts.trivago.com/login#'
+end
+
+When(/^Press the "([^"]*)" button in login page$/) do |buttonName|
+  find(:xpath, '//*[@id="authentication-login"]/div/section/div[3]/div/button/span[1]').click
+end
+
+Then(/^I should see an "([^"]*)" message "([^"]*)"$/) do |roleMessage, message|
+    find(:xpath,'//*[@id="authentication-login"]/div/section/div[3]/div/div[2]' ,:text => message)
+end
+
+Given(/^I enter a correct user and password$/) do
+  fill_in 'emailLogin', :with => ENV['USER']
+  fill_in 'login-pass', :with => ENV['PSW']
+end
+
+Then(/^I should see the dashboard for the logged account$/) do
+  find(:xpath, '/html/body/div/div/div/h1', {:class => 'fw-normal accounts__header', :text => 'My dashboard'})
+end
+
+Then(/^I visit the homepage$/) do
+  visit 'http://trivago.com/en'
+end
+
+Then(/^I can see my account on top\-right of screen$/) do
+  have_content ENV['USER']
+end
