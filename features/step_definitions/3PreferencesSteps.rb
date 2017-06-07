38bd1ced4a46918b2a97d020409f70350f62088f
sergioKmoralesV <s.mora.94@gmail.com>
2017-06-07 06:25:21 -0400

    First scenario for preferences added

--- features/step_definitions/3PreferencesSteps.rb
+++ features/step_definitions/3PreferencesSteps.rb
@@ -6,6 +6,7 @@ Then(/^I can see how may of the links have changed into Spanish$/) do
   have_content ('hotel')
   find(:xpath,'//*[@id="js_navigation"]/div/div[3]/button/span', :text => 'ES')
   find(:xpath,'//*[@id="js_navigation"]/div/div[4]/button/span', :text => 'Mi perfil')
+  find(:xpath,'//*[@id="js-fullscreen-hero"]/div/form/div[1]/div[2]/div/div[2]/button/span/span', :text => 'Buscar')
   have_content ('Mi perfil')
   have_content ('Términos de uso')
   have_content ('Ayuda')
